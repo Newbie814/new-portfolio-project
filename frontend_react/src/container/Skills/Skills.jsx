1ac182d3f4be677337a055a456d70561c0e03085
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-05-18 22:10:11 -0400

    finished work section tag functionality. Setting foundation for skills section

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+
+import ReactToolTip from
+
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
+import './Work.scss';
 
 import './Skills.scss';
 
